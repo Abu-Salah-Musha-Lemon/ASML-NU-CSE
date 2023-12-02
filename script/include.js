aa41af7036c8c515200675be947c9de43fde036b
Abu-Salah-Musha-Lemon <lemonahmed512@gmail.com>
2023-12-02 22:24:52 +0600

    update

--- script/include.js
+++ script/include.js
@@ -33,3 +33,13 @@ document.onkeydown = (e) => {
   )
     return false;
 };
+
+const activePage = window.location.href; // it is used for finding the location path of the current page
+console.log(activePage);
+const navLinks = document.querySelectorAll('.navbar a').forEach(link => {
+  if (link.href.includes(`${activePage}`)) {
+    // console.log(link.href);
+    link.classList.add('active');
+    // console.log(activePage); // Corrected the variable name here
+  }
+});
