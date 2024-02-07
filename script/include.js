fabc394932951b3dc742462008244f20cc3e06e6
Abu-Salah-Musha-Lemon <lemonahmed512@gmail.com>
2024-02-07 09:30:38 +0600

    update

--- script/include.js
+++ script/include.js
@@ -34,12 +34,33 @@ document.onkeydown = (e) => {
     return false;
 };
 
-const activePage = window.location.href; // it is used for finding the location path of the current page
-console.log(activePage);
-const navLinks = document.querySelectorAll('.navbar a').forEach(link => {
-  if (link.href.includes(`${activePage}`)) {
-    // console.log(link.href);
-    link.classList.add('active');
-    // console.log(activePage); // Corrected the variable name here
-  }
+// const activePage = window.location.href; // it is used for finding the location path of the current page
+// console.log(activePage);
+// const navLinks = document.querySelectorAll('.navbar-collapse .navbar-nav .nav-Item a').forEach(link => {
+//   if (link.href.includes(`${activePage}`)) {
+//     // console.log(link.href);
+//     link.classList.add('active');
+//     // console.log(activePage); // Corrected the variable name here
+//   }
+// });
+
+// const activePage = window.location.href;
+// console.log(activePage);
+// const navLinks = document.querySelectorAll('.navbar-collapse .navbar-nav .nav-item a').forEach(link => {
+//   if (link.href.toLowerCase() === activePage.toLowerCase()) {
+//     link.classList.add('active');
+//   }
+// });
+
+// this function work properly. Page active 
+document.addEventListener("DOMContentLoaded", function () {
+  const activePage = window.location.href;
+
+  const navLinks = document.querySelectorAll('.navbar-collapse .navbar-nav .nav-item a');
+
+  navLinks.forEach(link => {
+    if (link.href === activePage) {
+      link.classList.add('active');
+    }
+  });
 });
