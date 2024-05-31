a545278ce1842e239b664300e29eb391f028c7ec
Abu Salah Musha Lemon <lemonahmed512@gmail.com>
2024-05-31 22:23:37 +0600

    update

--- script/include.js
+++ script/include.js
@@ -47,20 +47,3 @@ document.onkeydown = (e) => {
     return false;
 };
 
-// const activePage = window.location.href; // it is used for finding the location path of the current page
-// console.log(activePage);
-// const navLinks = document.querySelectorAll('.navbar-collapse .navbar-nav .nav-Item a').forEach(link => {
-//   if (link.href.includes(`${activePage}`)) {
-//     // console.log(link.href);
-//     link.classList.add('active');
-//     // console.log(activePage); // Corrected the variable name here
-//   }
-// });
-
-// const activePage = window.location.href;
-// console.log(activePage);
-// const navLinks = document.querySelectorAll('.navbar-collapse .navbar-nav .nav-item a').forEach(link => {
-//   if (link.href.toLowerCase() === activePage.toLowerCase()) {
-//     link.classList.add('active');
-//   }
-// });
